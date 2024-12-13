4654d248f4149a5078bf73b2f537618d0191f82d
Thoshinijalakam <thoshinijalakam@gmail.com>
2024-12-13 15:28:00 +0530

    Add files via upload

---
+++ Mapinterface.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Mapinterface {
+    public static void main(String args[]){
+        HashMap<String,String>a=new HashMap<>();
+        a.put("SNo","01");
+        a.put("SName","Thoshii");
+        System.out.println(a);
+        
+    }
+    
+}
