c1d5b81190a9a6814327d6510515a76364b1f5ab
Thoshinijalakam <thoshinijalakam@gmail.com>
2024-12-13 12:40:07 +0530

    Add files via upload

---
+++ Listinterface.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Listinterface {
+    public static void main(String args[]){
+        LinkedList<Integer>a=new LinkedList<>();
+        Scanner sc=new Scanner(System.in);
+        int size=sc.nextInt();
+        for(int i=0;i<size;i++){
+                int s=sc.nextInt();
+                a.addFirst(s);
+            }
+            for(int i=0;i<size;i++)
+            System.out.println(a.get(i)+" ");
+    }
+    
+}
+/*[Hello, i am, Students]
+[i am, Students]
+[]
+PS C:\Users\Student.DESKTOP-P*/
