90bdedf9b30bdbd95e46f84a4b9e15299c1d295e
Thoshinijalakam <thoshinijalakam@gmail.com>
2024-12-13 12:39:40 +0530

    Add files via upload

---
+++ Printlinkedhashset.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Printlinkedhashset {
+    public static void main(String args[]){
+        LinkedHashSet<String>a=new LinkedHashSet<>();
+        a.add("Hello");
+    a.add("hello");
+a.add("Students");
+a.add("rajesh");
+a.add("Students");
+for(String s:a){
+    System.out.println(s+" ");
+}    }
+    
+}
+/*Hello 
+hello 
+Students
+rajesh*/
